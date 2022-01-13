f62780c26f82debbfa73cdb3753dd6fc2100a44e
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-01-13 17:04:17 +0100

    vision2D: allow to check for undistorted frames after calibration.

--- vision2D.py
+++ vision2D.py
@@ -39,6 +39,8 @@ def cmdLineArgs():
                         help='calibration: NF frames, chessboard size (CX, CY), SS square size (mm)')
     parser.add_argument('--calibration-reload-frames', dest='reload', action='store_true',
                         help='reload frames, if any, used during previous calibration.')
+    parser.add_argument('--alpha', type=float, default=0., metavar='A',
+                        help='free scaling parameter used to compute camera matrix.')
     args = parser.parse_args()
 
     # Convert calibration parameters.
@@ -49,9 +51,8 @@ def cmdLineArgs():
 
     return args
 
-def calibrateCamera(args, obj, img, gray):
+def calibrateCamera(args, obj, img, shape):
     # Camera calibration.
-    shape = gray.shape[::-1]
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj, img, shape, None, None)
     videoName = '%s%d'%(args.videoType, args.videoID)
     fdh = h5py.File('%s.h5'%videoName, 'w')
@@ -65,6 +66,8 @@ def calibrateCamera(args, obj, img, gray):
     fdh.create_dataset('shape', data=shape)
     fdh.close()
 
+    return mtx, dist
+
 def chessboardCalibration(args, frame, obj, img):
     # Termination criteria.
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -77,18 +80,27 @@ def chessboardCalibration(args, frame, obj, img):
 
     # Find the chess board corners
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, corners = cv2.findChessboardCorners(gray, (cbY, cbX), None)
+    ret, corners = cv2.findChessboardCorners(gray, (cbX, cbY), None)
 
     # If found, add object points, image points (after refining them)
+    print(' Chessboard found %s' % ret, end='', flush=True)
     if ret == True:
+        print(': keep', flush=True)
         obj.append(objPt)
         img.append(corners)
 
         # Draw and display the corners
         cornersSP = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        cv2.drawChessboardCorners(frame, (cbY, cbX), cornersSP, ret)
+        frameWCC = frame.copy()
+        cv2.drawChessboardCorners(frameWCC, (cbX, cbY), cornersSP, ret)
+        cv2.imshow('Captured frame with found chessboard corners', frameWCC)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        nbc = 0 if not corners else len(corners)
+        print(': drop (%d corners)' % nbc, flush=True)
 
-    return ret, gray
+    return ret
 
 def main():
     # Get command line arguments.
@@ -97,57 +109,70 @@ def main():
     # Data needed for calibration.
     obj = [] # 3d point in real world space
     img = [] # 2d points in image plane.
-    gray = None
+    shape = None
     frames, videoName = [], '%s%d'%(args.videoType, args.videoID)
     if args.reload: # Reload previous frames on demand.
-        for frame in glob.glob(videoName + '-*.jpg'):
-            frames.append(frame)
+        print('Loading frames...', flush=True)
+        for fname in glob.glob(videoName + '-*.jpg'):
+            print('  Loading %s...' % fname, end='', flush=True)
+            frame = cv2.imread(fname)
+            ret = chessboardCalibration(args, frame, obj, img)
+            if ret:
+                frames.append(frame)
 
     # Capture video stream.
+    mtx, dist, newCamMtx = None, None, None
     vid = VideoStream(vars(args))
+    print('Capturing frames...', flush=True)
     while(vid.isOpened()):
         # Get video frame.
         frameOK, frame, fps = vid.read()
         if not frameOK:
             continue
+        height, width, channel = frame.shape
+        shape = (height, width)
 
         # Display the resulting frame.
-        print('FPS %d'%fps, flush=True)
-        cv2.imshow('Video [c capture, q quit]', frame)
+        print('  FPS %d'%fps, flush=True)
+        cv2.imshow('Video raw [c capture, q quit]', frame)
+        if mtx is not None and dist is not None and newCamMtx is not None:
+            undFrame = cv2.undistort(frame, mtx, dist, None, newCamMtx)
+            cv2.imshow('Video undistorted [q quit]', undFrame)
+
+        # Press 'q' to quit.
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'): # Press 'q' to quit.
+            print('  Exiting...', flush=True)
+            break
+        if frames is None:
+            continue
 
         # Wait for key from user according to displayed frame.
-        key = cv2.waitKey(1) & 0xFF
         if key == ord('c'): # Press 'c' to capture.
-            print('  Capturing frame %s...' % len(frames), end=' ', flush=True)
+            print('  Capturing frame %s...' % len(frames), end='', flush=True)
             cv2.destroyAllWindows()
             cv2.imshow('Captured frame: keep? [y/n]', frame)
             key = cv2.waitKey(0) & 0xFF
             if key == ord('y'):
                 cv2.destroyAllWindows()
-                ret, gray = chessboardCalibration(args, frame, obj, img)
-                print('Kept: chessboard found %s' % ret, flush=True)
-                cv2.imshow('Captured frame: chessboard found %s' % ret, frame)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+                ret = chessboardCalibration(args, frame, obj, img)
                 if ret == True:
                     frames.append(frame)
-            else:
-                print('Dropped', flush=True)
             cv2.destroyAllWindows()
-        elif key == ord('q'): # Press 'q' to quit.
-            print('Exiting...', flush=True)
-            break
 
         # Calibrate camera.
         if args.nbFrames == len(frames):
-            print('Calibrating...', flush=True)
-            calibrateCamera(args, obj, img, gray)
+            print('  Calibrating...', flush=True)
+            mtx, dist = calibrateCamera(args, obj, img, shape)
+            alpha = args.alpha
+            newCamMtx, roiCam = cv2.getOptimalNewCameraMatrix(mtx, dist, (width, height),
+                                                              alpha, (width, height))
 
             # Save frames used to calibrate.
-            print('Saving frames...', flush=True)
+            print('  Saving frames...', flush=True)
             for idx, frame in enumerate(frames):
                 cv2.imwrite(videoName + '-%02d.jpg'%idx, frame)
-            break
+            frames = None # Now calibration is done and frames are saved, no more capture.
 
     # After the loop release the video stream.
     print('Releasing video stream...', flush=True)
