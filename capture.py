050a031bde37bae71991d9e3977a111aebed3454
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2022-02-01 13:49:57 +0100

    capture: enable capture with N concurrent threads.

--- capture.py
+++ capture.py
@@ -12,6 +12,10 @@ import cv2
 # Synchronisation barrier.
 sync = None
 
+# Global variables used as signal events to connect/emit all threads to each others.
+saveEvent = False
+quitEvent = False
+
 def cmdLineArgs():
     # Create parser.
     dscr = 'script designed to capture frames from video stream for later calibration.'
@@ -34,16 +38,13 @@ class CaptureThread(threading.Thread):
         # Initialise.
         super().__init__()
         self._args = args.copy()
-        self._frame = None
         self._idxFrame = self._args['startIdx']
-        self._saveLock = threading.Lock()
-        self._saveFrame = False
-        self.otherThd = None
 
     def run(self):
         # Capture frames.
         vid = VideoStream(self._args)
         vidID = self._args['videoID']
+        global quitEvent, saveEvent
         while(vid.isOpened()):
             # Get video frame.
             frameOK, frame, fps = vid.read()
@@ -51,28 +52,31 @@ class CaptureThread(threading.Thread):
                 continue
 
             # Save frame. Show it.
-            self._frame = frame # Refresh frame.
             cv2.imshow('stream%02d - Video raw [s save, q quit]'%vidID, frame)
 
             # Wait for user action.
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'): # Press 'q' to quit.
+                quitEvent = True
+            if key == ord('s'): # Press 's' to save.
+                saveEvent = True
+
+            # Execute user action.
+            if quitEvent:
                 print('stream%02d: exiting...'%vidID, flush=True)
                 break
-            if key == ord('s') or self._saveFrame: # Press 's' to save, or, triggered by other.
-                if key == ord('s') and self.otherThd is not None:
-                    self.otherThd.triggerSave() # Caution: trigger only on press key.
-                self.onSave() # Both threads must take the same picture at the same time.
+            if saveEvent:
+                self.save(frame) # Both threads must take the same picture at the same time.
+                saveEvent = False # All threads reset save flag.
         vid.release()
         cv2.destroyAllWindows()
 
-    def onSave(self):
+    def save(self, frame):
         # Check VISUALLY if chessboards are PROPERLY found on BOTH frames: MANDATORY for correct stereo calibration.
-        self._saveLock.acquire()
         vidID = self._args['videoID']
         print('stream%02d: looking for chessboard...'%vidID, flush=True)
         obj, img = [], []
-        ret = chessboardCalibration(self._args, self._frame, obj, img, msg='stream%02d:'%vidID)
+        ret = chessboardCalibration(self._args, frame, obj, img, msg='stream%02d:'%vidID)
         sync.wait() # Wait for all chessboards (from all threads) to be checked.
 
         # Wait to know if chessboard corners are found properly.
@@ -80,21 +84,17 @@ class CaptureThread(threading.Thread):
         if ret:
             while key != 'y' and key != 'n':
                 key = input('stream%02d: keep? [y/n] '%vidID)
+        else:
+            print('stream%02d: drop'%vidID, flush=True)
         sync.wait() # Wait for all answers (from all threads): keep? drop?
+
+        # Save frame on demand.
         if key == 'y':
-            # Save frame.
             print('stream%02d: saving frame %02d...'%(vidID, self._idxFrame), flush=True)
             fileID = '%s%d'%(self._args['videoType'], self._args['videoID'])
-            cv2.imwrite(fileID + '-%02d.jpg'%self._idxFrame, self._frame)
+            cv2.imwrite(fileID + '-%02d.jpg'%self._idxFrame, frame)
             self._idxFrame += 1
-        self._saveFrame = False
-        self._saveLock.release()
-
-    def triggerSave(self):
-        # Trigger save as soon as possible.
-        self._saveLock.acquire()
-        self._saveFrame = True
-        self._saveLock.release()
+        sync.wait() # All threads wait for each others.
 
 def main():
     # Get command line arguments.
@@ -112,11 +112,6 @@ def main():
     nbThreads = 2 if strThd is not None else 1
     global sync
     sync = threading.Barrier(nbThreads)
-
-    # Create connection between threads.
-    if strThd is not None: # Both threads must take the same picture at the same time.
-        mainThd.otherThd = strThd
-        strThd.otherThd = mainThd
 
     # Start threads.
     mainThd.start()
