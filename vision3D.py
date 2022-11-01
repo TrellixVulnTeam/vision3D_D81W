9f262d26c6644ad1a2cca01ad413fc984b242483
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 02:16:22 +0000

    Adding tarfile member sanitization to extractall()

--- vision3D.py
+++ vision3D.py
@@ -578,7 +578,26 @@ class Vision3D(QWidget):
                                                 dest_path='./models_VGGNet_coco_SSD_512x512.tar.gz',
                                                 showsize=True, unzip=False)
             with tarfile.open('models_VGGNet_coco_SSD_512x512.tar.gz') as tgz:
-                tgz.extractall('./models_VGGNet_coco_SSD_512x512')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tgz, "./models_VGGNet_coco_SSD_512x512")
         else:
             logger.info('[vision3D] models_VGGNet_coco_SSD_512x512.tar.gz has already been downloaded.')
 
